219fd3dbab7ba6eb15def1fc23ed7815713232ff
Supachai Wijaiya <supachaiwijaiya@gmail.com>
2026-02-17 08:55:08 +0700

    Initial commit of the project”

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,81 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // สำหรับทำ Form
+import { Person, PersonService } from './person.service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // Import Modules 
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('my-tets-angular');
+export class AppComponent implements OnInit {
+  people: Person[] = [];
+
+  // --- Modal ---
+  showAddModal: boolean = false;   // ควบคุม Modal เพิ่มข้อมูล
+  showViewModal: boolean = false;  // ควบคุม Modal ดูข้อมูล
+  selectedPerson: Person | null = null; // เก็บข้อมูลคนที่จะดู
+
+  
+  newPerson: Person = {
+    firstName: '',
+    lastName: '',
+    address: '',
+    birthDate: ''
+  };
+
+  constructor(private personService: PersonService) { }
+
+  ngOnInit() {
+    this.loadPeople();
+  }
+
+  loadPeople() {
+    this.personService.getPeople().subscribe(data => {
+      this.people = data;
+    });
+  }
+
+  // --- Managed Modal Add ---
+  openAddModal() { this.showAddModal = true; }
+  closeAddModal() { this.showAddModal = false; }
+
+  saveNewPerson() {
+    if (!this.newPerson.firstName) return;
+    this.personService.addPerson(this.newPerson).subscribe(() => {
+      this.loadPeople();
+      this.closeAddModal(); 
+      this.newPerson = { firstName: '', lastName: '', address: '', birthDate: '' }; // Clear
+    });
+  }
+
+  get calculatedAge(): string {
+    if (!this.newPerson.birthDate) return '-'; // ถ้ายังไม่เลือกวัน ให้ขีดไว้
+
+    const birth = new Date(this.newPerson.birthDate);
+    const today = new Date();
+
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+
+    // เช็คว่าถึงวันเกิดปีนี้หรือยัง ถ้ายังให้ลบ 1 ปี
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+
+    return age.toString();
+  }
+
+  // --- ManageD Modal GET---
+  openViewModal(person: Person) {
+    this.selectedPerson = person; 
+    this.showViewModal = true;
+  }
+  closeViewModal() {
+    this.showViewModal = false;
+    this.selectedPerson = null;
+  }
 }
---
+++ src/app/person.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Person {
+  id?: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  birthDate: string;
+  age?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PersonService {
+
+  private apiUrl = 'http://localhost:5209/people'; // Check Port  API .NET
+
+  constructor(private http: HttpClient) { }
+
+  getPeople(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.apiUrl);
+  }
+
+  addPerson(person: Person): Observable<Person> {
+    return this.http.post<Person>(this.apiUrl, person);
+  }
+
+  deletePerson(id: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
